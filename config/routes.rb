946e9e4bc8fc227535bd4cdd4ccf5e6efcf2bd93
devadrsilva <adriano.mssilva1@gmail.com>
2023-08-08 19:05:51 -0300

    criação de parametros protegidos

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "articles#index"
+  # root vai ser usado para definir a página principal
+  
+  
+resources :articles
+ 
+  
 end
